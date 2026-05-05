3909f5d1f5dc37f9cb6bb87c9af6ebdfdcdf8f2e
Joao-Marcelo-Costa <joaomacelocosta27@gmail.com>
2026-05-05 11:17:42 -0300

    conserta estilo de tarefas

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,9 @@
 const btAdicionarTarefa = document.querySelector(".app__button--add-task")
 const formAdicionarTarefa = document.querySelector(".app__form-add-task")
 const textArea = document.querySelector(".app__form-textarea")
-
-const listaDeTarefas = []
+const ulTarefas = document.querySelector(".app__section-task-list")
+const listaDeTarefas = JSON.parse(localStorage.getItem("Tarefas")) || []
+console.log(listaDeTarefas)
 
 function criarElementoTarefa (tarefa){ //função que transforma uma tarefa object em um HTML dessa tarefa
     const li = document.createElement("li")
@@ -10,12 +11,10 @@ function criarElementoTarefa (tarefa){ //função que transforma uma tarefa obje
 
     const svg = document.createElement("svg")
     svg.innerHTML = `
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `
     const paragrafo = document.createElement("p")
     paragrafo.classList.add("app__section-task-list-item-description")
@@ -31,6 +30,8 @@ function criarElementoTarefa (tarefa){ //função que transforma uma tarefa obje
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+    return li
 }
 
 let contagemDeTarefas = 0
@@ -47,3 +48,8 @@ formAdicionarTarefa.addEventListener("submit", (evento)=>{
     localStorage.setItem(`Tarefas`,JSON.stringify(listaDeTarefas))
     contagemDeTarefas ++
 })
+
+listaDeTarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+});
