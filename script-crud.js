15fad15a67593f35817bf783824ac136605caccc
Joao-Marcelo-Costa <joaomacelocosta27@gmail.com>
2026-05-05 10:27:19 -0300

    cria a função que tranforma o objeto lista em HTML

--- script-crud.js
+++ script-crud.js
@@ -1,7 +1,37 @@
 const btAdicionarTarefa = document.querySelector(".app__button--add-task")
 const formAdicionarTarefa = document.querySelector(".app__form-add-task")
 const textArea = document.querySelector(".app__form-textarea")
+
 const listaDeTarefas = []
+
+function criarElementoTarefa (tarefa){ //função que transforma uma tarefa object em um HTML dessa tarefa
+    const li = document.createElement("li")
+    li.classList.add("app__section-task-list-item")
+
+    const svg = document.createElement("svg")
+    svg.innerHTML = `
+    <svg>
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    </svg>
+    `
+    const paragrafo = document.createElement("p")
+    paragrafo.classList.add("app__section-task-list-item-description")
+    paragrafo.textContent = tarefa.descricao
+
+    const botao = document.createElement('button')
+    botao.classList.add("app_button-edit")
+    
+    const imagemBotao = document.createElement("img")
+    imagemBotao.setAttribute("src", "/imagens/edit.png" )
+    botao.append(imagemBotao)
+
+    li.append(svg)
+    li.append(paragrafo)
+    li.append(botao)
+}
 
 let contagemDeTarefas = 0
 btAdicionarTarefa.addEventListener("click", () =>{
@@ -10,10 +40,10 @@ btAdicionarTarefa.addEventListener("click", () =>{
 
 formAdicionarTarefa.addEventListener("submit", (evento)=>{
     evento.preventDefault()
-    const tarefa = {
-        descricao: textArea.value
+    const tarefa = { 
+    descricao: textArea.value //cria um objeto chamado tarefa com um índice descrição
     }
-    listaDeTarefas.push(tarefa)
-    localStorage.setItem(`Tarefa ${contagemDeTarefas}`,JSON.stringify(listaDeTarefas))
+    listaDeTarefas.push(tarefa) //coloca o objeto chamado tarefa na lista de tarefas
+    localStorage.setItem(`Tarefas`,JSON.stringify(listaDeTarefas))
     contagemDeTarefas ++
 })
